5cac8de49c6ab1720611f0b7c0c7041f99e6ffcd
bsb-github <codewithsheri64@gmail.com>
2022-02-03 21:16:08 +0500

    SignUp

--- App.js
+++ App.js
@@ -1,187 +1,226 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import signUpScreen from './screens/signUp';
+import React, {useState} from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  Alert,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignUpScreen from "./screens/signUp";
+import HomeScreen from "./screens/homeScreen";
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const Stack = createNativeStackNavigator();
+
+function SignIn({ navigation }) {
+  const auth = getAuth();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+  const onpressed = () => {
+    navigation.navigate("SignUp");
+  };
  
-export default function App() {
-  
-  const Stack = createNativeStackNavigator();
-<Stack.Screen
-  name="Home"
-  component={signUpScreen}
-  options={{ title: 'Overview' }}
-/>
+  const [Email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserSignIn = () => {
+    signInWithEmailAndPassword(auth, Email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user.email)
+    navigation.navigate("home");
+    // ...
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    <Alert>{errorMessage}</Alert>
+  });
+
+  };
   return (
-    
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.marginAbove}>
-      <Text style={styles.textStyle} 
-      >Sign In</Text>
+        <Text style={styles.textStyle}>Sign In</Text>
       </View>
-      <View
-      style={
-        styles.spacer
-      }
-      ></View>
+      <View style={styles.spacer}></View>
       <View style={styles.container2}>
         <TextInput
-        placeholder='Username.......'
-        style={styles.loginTextInput}
+          placeholder="Email......."
+
+          placeholderTextColor={"#46cd80"}
+          value={Email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.loginTextInput}
         />
       </View>
       <View style={styles.container3}>
         <TextInput
-        placeholder='Password.......'
-        style={styles.loginTextInput}
-        secureTextEntry= {true}
+          placeholder="Password......."
+          placeholderTextColor={"#46cd80"}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.loginTextInput}
+          secureTextEntry={true}
         />
       </View>
       <View style={styles.buttonSignIn}>
-        <Button 
-        title='Sign In'
-        color={'#46cd80'}
-        style={styles.buttonView}
-        />
-      </View>
-      <View
-      style={styles.spacer2}
-      >
-      </View>
-      <View style={styles.donthaveAccount}>
-        <Text style={styles.textStyle1}>Dont Have Account?</Text>
-        <TouchableOpacity
-        onPress={() => navigation.navigate('Home')}
-        >
-        <Text style={styles.textStyle2}>  Sign Up</Text>
+        <TouchableOpacity style={styles.buttonSignInBg} onPress={handleUserSignIn}>
+          <Text style={styles.blackText}>Sign In</Text>
         </TouchableOpacity>
       </View>
-      <View
-      style={styles.spacer2}
-      >
+      <View style={styles.spacer2}></View>
+      <View style={styles.donthaveAccount}>
+        <Text style={styles.textStyle1}>Dont Have Account?</Text>
+        <TouchableOpacity onPress={onpressed}>
+          <Text style={styles.textStyle2}> Sign Up</Text>
+        </TouchableOpacity>
       </View>
+      <View style={styles.spacer2}></View>
       <View style={styles.spacer3}>
         <TouchableOpacity>
           <Text style={styles.textStyle3}>Forgot Password?</Text>
         </TouchableOpacity>
-        
       </View>
       <View style={styles.spacer3}>
         <Text style={styles.textStyle1}>OR</Text>
       </View>
       <View style={styles.signInOption}>
-        <Button
-        title='Google'
-        />
-        <View
-        style={styles.spacer4}
-        ></View>
+        <Button title="Google" />
+        <View style={styles.spacer4}></View>
       </View>
-      
-    </View>
+    </SafeAreaView>
   );
 }
-function navi() {
+
+export default function App() {
+ // Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBg56fI1Hqj5ZL1i18i5ToV7nRyq3fz3F4",
+  authDomain: "fir-auth-91a42.firebaseapp.com",
+  databaseURL: "https://fir-auth-91a42-default-rtdb.firebaseio.com",
+  projectId: "fir-auth-91a42",
+  storageBucket: "fir-auth-91a42.appspot.com",
+  messagingSenderId: "444509202678",
+  appId: "1:444509202678:web:5716e682ed960abc3824d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="signUp" component={signUpScreen} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 const styles = StyleSheet.create({
-  img:{
-    height: 50,
-    width: 50
+  blackText: {
+    color: "#000000",
+    fontSize: 20,
   },
   container: {
     flex: 1,
-    backgroundColor: '#1c213b',
+    backgroundColor: "#1c213b",
   },
   textStyle: {
-    color: '#efefef',
+    color: "#efefef",
     fontSize: 25,
   },
   textStyle1: {
-    color: '#efefef',
+    color: "#efefef",
     fontSize: 16,
   },
   textStyle2: {
-    color: '#46cd80',
+    color: "#46cd80",
     fontSize: 16,
   },
   textStyle3: {
-    color: '#46cd80',
+    color: "#46cd80",
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   marginAbove: {
     marginTop: 15,
-    marginLeft: 15
-
+    marginLeft: 15,
   },
-  container2:{
+  container2: {
     marginTop: 30,
     margin: 16,
-    backgroundColor: '#252849',
+    backgroundColor: "#252849",
     height: 50,
-//    alignItems: 'center',
-    justifyContent: 'center',
+    //    alignItems: 'center',
+    justifyContent: "center",
     padding: 34,
-    borderRadius: 30
-
+    borderRadius: 30,
   },
-  loginTextInput:{
+  loginTextInput: {
     height: 50,
-    color: '#fff',
+    color: "#fff",
     fontSize: 20,
-    
   },
-  spacer:{
-    marginTop: 40
+  spacer: {
+    marginTop: 40,
   },
-  spacer2:{
-    marginTop: 16
+  spacer2: {
+    marginTop: 16,
   },
-  spacer3:{
+  spacer3: {
     marginTop: 20,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
-  spacer4:{
+  spacer4: {
     margin: 10,
   },
-  container3:{
+  container3: {
     marginTop: 20,
     margin: 16,
-    backgroundColor: '#252849',
+    backgroundColor: "#252849",
     height: 50,
-//    alignItems: 'center',
-    justifyContent: 'center',
+    //    alignItems: 'center',
+    justifyContent: "center",
     padding: 34,
-    borderRadius: 30
-
+    borderRadius: 30,
   },
-  buttonView:{ 
-    height: 50
-
-   },
-  buttonSignIn:{
-    margin: 16,
-    marginHorizontal: 80,
-   borderRadius: 20
-
+  buttonView: {
+    height: 50,
   },
-  donthaveAccount:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+  buttonSignIn: {
+    margin: 18,
+    borderRadius: 20,
   },
-  signInOption:{
-    flexDirection: 'row',
+  donthaveAccount: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signInOption: {
+    flexDirection: "row",
     marginTop: 40,
-    alignContent:'center',
-    justifyContent: 'center'
-  }
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  buttonSignInBg: {
+    height: 55,
+    backgroundColor: "#46cd80",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+  },
 });
--- screens/signUp.js
+++ screens/signUp.js
@@ -1,15 +1,218 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native"
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { initializeApp } from "firebase/app";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
 
-export default function signUpScreen(){
-    return(
+const Stack = createNativeStackNavigator();
+
+export default function SignUpScreen({ navigation }) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBg56fI1Hqj5ZL1i18i5ToV7nRyq3fz3F4",
+    authDomain: "fir-auth-91a42.firebaseapp.com",
+    projectId: "fir-auth-91a42",
+    databaseURL: "https://fir-auth-91a42-default-rtdb.firebaseio.com/",
+    storageBucket: "fir-auth-91a42.appspot.com",
+    messagingSenderId: "444509202678",
+    appId: "1:444509202678:web:5716e682ed960abc3824d7",
+  };
+
+  //const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const [Email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  // const database = getDatabase(app);
+
+  const handleSignUp = () => {
+    let userId = 1;
+    const db = getDatabase();
+    set(ref(db, "users/" + username), {
+      username: username,
+      email: Email,
+    });
+    createUserWithEmailAndPassword(auth, Email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user.email);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  };
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+  const onpressed = () => {
+    navigation.goBack();
+  };
+  var back = "<-";
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.signup}>
         <View>
-            <Text>sIGNuP</Text>
+          <Text
+            style={{
+              color: "#efefefef",
+              fontSize: 28,
+              fontWeight: "bold",
+            }}
+          >
+            Sign Up
+          </Text>
         </View>
-    );
+        <View>
+          <TouchableOpacity
+            style={{
+              marginTop: 8,
+              justifyContent: "space-between",
+              flexDirection: "row",
+            }}
+          >
+            <Text
+              style={{
+                color: "#efefefef",
+                fontSize: 16,
+              }}
+            >
+              {back}
+            </Text>
+
+            <View
+              style={{
+                marginRight: 4,
+              }}
+            ></View>
+
+            <Text
+              style={{
+                color: "#efefefef",
+                fontSize: 16,
+              }}
+            >
+              Back To Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.spacer}></View>
+      <View style={styles.container2}>
+        <TextInput
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          placeholder="Username......."
+          placeholderTextColor={"#46cd80"}
+          style={styles.loginTextInput}
+        />
+      </View>
+      <View style={styles.container3}>
+        <TextInput
+          value={Email}
+          onChangeText={(text) => setEmail(text)}
+          placeholder="Email......."
+          placeholderTextColor={"#46cd80"}
+          style={styles.loginTextInput}
+        />
+      </View>
+      <View style={styles.container3}>
+        <TextInput
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder="Password......."
+          placeholderTextColor={"#46cd80"}
+          style={styles.loginTextInput}
+          secureTextEntry={true}
+        />
+      </View>
+      <View>
+        <View style={styles.buttonSignIn}>
+          <TouchableOpacity
+            style={styles.buttonSignInBg}
+            onPress={handleSignUp}
+          >
+            <Text style={styles.blackText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.spacer3}>
+        <Text style={styles.textStyle1}>OR</Text>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#1c213b",
+    paddingTop: 30,
+    paddingHorizontal: 26,
+  },
+  signup: {
+    flexDirection: "row",
+    alignContent: "space-between",
+    justifyContent: "space-between",
+  },
+  spacer: {
+    marginTop: 40,
+  },
+  container2: {
+    marginTop: 30,
+    margin: 16,
+    backgroundColor: "#252849",
+    height: 50,
+    //    alignItems: 'center',
+    justifyContent: "center",
+    padding: 34,
+    borderRadius: 30,
+  },
+  loginTextInput: {
+    height: 50,
+    color: "#fff",
+    fontSize: 20,
+  },
+  container3: {
+    marginTop: 20,
+    margin: 16,
+    backgroundColor: "#252849",
+    height: 50,
+    //    alignItems: 'center',
+    justifyContent: "center",
+    padding: 34,
+    borderRadius: 30,
+  },
+  buttonSignIn: {
+    margin: 18,
+    borderRadius: 20,
+  },
+  buttonSignInBg: {
+    height: 55,
+    backgroundColor: "#46cd80",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+  },
+  blackText: {
+    color: "#000000",
+    fontSize: 20,
+  },
+  spacer3: {
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
